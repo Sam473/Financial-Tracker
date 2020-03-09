10f527c00675b0ab783d0fe2962e72430eeeecef
Lauren Pococke <laurenpococke@gmail.com>
2020-03-09 17:07:12 +0000

    Update Category.java

--- Category.java
+++ Category.java
@@ -9,19 +9,28 @@ public class Category {
     private double totalAmountSpent;
     private String categoryName;
 
+    /*A new object of this class will be created for each
+    new category*/
     public Category(String name, int budgetValue) {
         this.budget = budgetValue;
         this.categoryName = name;
     }
 
+    /*User can set a monthly budget for this specific
+    category - this can be changed by the user*/
     public void newBudget(double budget){
         this.budget = budget;
     }
 
+    /*When the user adds a purchase, they select a
+    pre-existing category. This value spent is added
+    to the total amount spent in this category.*/
     public void addExpenditure(double amountSpent){
         this.totalAmountSpent += amountSpent;
     }
 
+    /*Incase the user writes a spelling mistake initally,
+    they can change the category name.*/
     public void changeCategoryName(String newName){
         this.categoryName = newName;
     }
