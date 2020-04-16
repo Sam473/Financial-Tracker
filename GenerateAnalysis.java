9898d3c59d6505e57026e6e98c8391899e34aa25
Samuel Stacey <s.stacey@live.co.uk>
2020-04-16 14:43:06 +0100

    New graph and size adjusted
    Pie chart for outgoings per month and method def for the changeable time period graph. Changed the sizes of the graphs as they were too big

--- GenerateAnalysis.java
+++ GenerateAnalysis.java
@@ -16,14 +16,13 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
 
-//Change this so it works with our data
 //Add some more charts
 //Call a method to delete all charts once program closes for data security
 
 public class GenerateAnalysis {
 	JFrame frame;
 	
-	public void CategoryPie() throws IOException {
+	public void purchaseCategoryPie() throws IOException {
 		DefaultPieDataset dataset = new DefaultPieDataset( );
 		
 		ResultSet rs = RetrieveAndStore.readAllRecords("tblCategory");
@@ -39,7 +38,7 @@ public class GenerateAnalysis {
 		}
 
 	    JFreeChart chart = ChartFactory.createPieChart(
-	       "Categories",   // chart title
+	       "Expenditure per Category (GBP)",   // chart title
 	       dataset,          // data
 	       true,             // include legend
 	       true,
@@ -50,8 +49,8 @@ public class GenerateAnalysis {
 	    int fontSize = 30; //the size of the fonts
 	    plot.setLabelFont(new Font("SansSerif", Font.PLAIN, fontSize));
 	    
-	    int width = 1280;   /* Width of the image */
-	    int height = 720;  /* Height of the image */ 
+	    int width = 848;   // Width of the image
+	    int height = 480;  // Height of the image
 	    File pieChart = new File("PieChart.png"); 
 	    ChartUtilities.saveChartAsPNG(pieChart, chart ,width ,height);
 	    showGraph("PieChart.png");
@@ -68,8 +67,48 @@ public class GenerateAnalysis {
 		  frame.setVisible(true);
 	}
 	
+	public void purchasesOverTimePeriodBar(int timePerod) {
+		//Need purchases to be implemented with valid dates to make this work
+		//Make work for weekly?, monthly, yearly purchases.
+	}
+	
+	public void regularOutgoingsPie() throws IOException {
+DefaultPieDataset dataset = new DefaultPieDataset( );
+		
+		ResultSet rs = RetrieveAndStore.readAllRecords("tblOutgoings");
+		try {
+			while (rs.next()) // Loop through the resultset
+			{
+				//Add each category to the dataset for the pie chart
+				dataset.setValue(rs.getString("OutgoingName"), rs.getInt("OutgoingAmount"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	    JFreeChart chart = ChartFactory.createPieChart(
+	       "Outgoings per Month (GBP)",   // chart title
+	       dataset,          // data
+	       true,             // include legend
+	       true,
+	       false);
+	    
+	    //Adjust the font size
+	    PiePlot plot = (PiePlot) chart.getPlot();
+	    int fontSize = 30; //the size of the fonts
+	    plot.setLabelFont(new Font("SansSerif", Font.PLAIN, fontSize));
+	    
+	    int width = 848;   // Width of the image
+	    int height = 480;  // Height of the image
+	    File pieChart = new File("PieChart2.png"); 
+	    ChartUtilities.saveChartAsPNG(pieChart, chart ,width ,height);
+	    showGraph("PieChart2.png");
+	}
 	
 	public void mainMenu () throws IOException {
-		CategoryPie();
+		purchaseCategoryPie(); //Categories they spend in
+		regularOutgoingsPie(); //The regular outgoings as a portion such as rent, bills, netflix, ...
+		//purchasesOverTimePeriodBar(1); //Comparison of purchases per unit of time decided by user
     }
 }
