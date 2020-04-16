900722425eef4f893b3c440ff6944b7794bb9795
Samuel Stacey <s.stacey@live.co.uk>
2020-04-16 19:15:40 +0100

    Implementing changeable timeframe graph

--- GenerateAnalysis.java
+++ GenerateAnalysis.java
@@ -15,6 +15,8 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class GenerateAnalysis {
 	JFrame frame;
@@ -64,9 +66,82 @@ public class GenerateAnalysis {
 		  frame.setVisible(true);
 	}
 	
-	public void purchasesOverTimePeriodBar() {
+	public void purchasesOverTimePeriodBar() throws IOException {
 		//Need purchases to be implemented with valid dates to make this work
 		//Make work for weekly?, monthly, yearly purchases.
+		String timePeriod;
+		int weeks[] = new int[10];
+		int months[] = new int[12];
+		int years[] = new int[5];
+		System.out.println(
+				"Time Period:\n1. Weekly\n2. Monthly\n3. Yearly\n4. Quit to main menu");
+		String input = App.userIn.readLine();
+		
+		switch(input) {
+		case "1":
+			timePeriod = "Weekly"; //Categories they spend in
+			break;
+		case "2":
+			timePeriod = "Monthly"; //The regular outgoings as a portion such as rent, bills, netflix, ...
+			break;
+		case "3":
+			timePeriod = "Yearly"; //Comparison of purchases per unit of time decided by user
+			break;
+		}
+		
+		final DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+		
+		ResultSet rs = RetrieveAndStore.readAllRecords("tblCategory");
+		try {
+			while (rs.next()) // Loop through the resultset
+			{
+				String purchaseDate = rs.getString("PurchaseDate");
+				int amount = rs.getInt("PurchaseAmount");
+				
+				switch(timePeriod) {
+				case "Weekly":
+					
+					break;
+				case "Monthly":
+					//add amount to array index of the month-1
+					int month = Integer.parseInt(purchaseDate.substring(3, 5));
+					months[month-1] += amount;
+					break;
+				case "Yearly":
+					break;
+				default:
+					return;
+				}
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+		for (int i = 0; i<11; i++) {
+			dataset.addValue
+		}
+		dataset.addValue(1, rs.getString("CategoryName"), rs.getInt("Expenditure"));
+
+		
+		
+		
+	      JFreeChart barChart = ChartFactory.createBarChart(
+	    	         "Purchases Comparison", 
+	    	         timePeriod, "Value", 
+	    	         dataset,PlotOrientation.VERTICAL, 
+	    	         true, true, false);
+	    	         
+	    	      int width = 640;    /* Width of the image */
+	    	      int height = 480;   /* Height of the image */ 
+	    	      File BarChart = new File( "BarChart.jpeg" ); 
+	    	      ChartUtilities.saveChartAsJPEG( BarChart , barChart , width , height );
+		
+		
+		
+		
+		
 	}
 	
 	public void regularOutgoingsPie() throws IOException {
